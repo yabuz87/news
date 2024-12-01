aaa43368ff665a31b0e978e1f8335b319767ba27
Yabets solomon <jabezsolomonz@gmail.com>
2024-12-01 14:17:54 +0300

    "Removed .env file, updated package-lock.json, added axios dependency, modified NewsArticle and NewsBoard components"

--- News/src/component/NewsArticle.jsx
+++ News/src/component/NewsArticle.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const NewsArticle = ({title,src,discription,url,}) => {
+const NewsArticle = ({title,src,discription,url}) => {
   return (
-    <div className="card" style="width: 18rem;">
+    <div className="card" style={{maxWidth:"345px"}}>
   <img src={src} className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">{title}</h5>
     <p className="card-text">{discription ? discription : "some thing here"}</p>
-    <a href={url} className="btn btn-primary">Read More</a>
+    <a href={url}  target="_blank" className="btn btn-primary">Read More</a>
   </div>
 </div>
   )
--- News/src/component/NewsBoard.jsx
+++ News/src/component/NewsBoard.jsx
@@ -1,25 +1,30 @@
-import React,{useState,useEffect} from 'react'
-import data from "./fetchData";
+import React, { useState, useEffect } from 'react';
 import NewsArticle from './NewsArticle';
-const NewsBoard = () => {
-  const [aricles,setAritcle]=useState([]);
-    useEffect(()=>{
-        let url=`GET https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.myapi}`;
-        fetch(url).then(response=>response.json()).then(data=> setAritcle(data.articles))
-    },[]);
 
-  data();
+const NewsBoard = () => {
+  const [articles, setArticles] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+        let url=`https://newsapi.org/v2/top-headlines?country=us&apiKey=dd8e0d11dff34c5aba5d5d5ec5d3e0de`;
+        fetch(url).then(response=>response.json()).then(data=>setArticles(data.articles));
+       { articles.map((news,index)=>{
+        return <NewsArticle key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url}/>
+       });}  
+  },[]);
+
   return (
     <div>
       <h1 className="text-center">Latest<span className="badge bg-danger py-2 m-2">News</span></h1>
-      {/* {
-        aricles.map((item,index)=>{
-          return(
-            <NewsArticle key={index} title={item.title} src={item.urlToImage} discription={item.discription} url={item.url}/>
+      {
+        articles.map((item, index) => {
+          return (
+            <NewsArticle key={index} title={item.title} src={item.urlToImage} description={item.description} url={item.url} />
           );
         })
-        } */}
-
-    </div>)}
+      }
+    </div>
+  );
+};
 
 export default NewsBoard;
